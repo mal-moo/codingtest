1eee2d0b3dac6bf816bcc636ae83d57ac0c69b44
mal-moo <malinmoohwagwa@gmail.com>
2022-08-25 18:24:42 +0900

    Add 07-04-problem-10828.py

---
+++ 07-stack/07-04-problem-10828.py
@@ -0,0 +1,34 @@
+"""
+    data structure: #stack
+    understand: y
+    link: https://www.acmicpc.net/problem/10828
+"""
+
+import sys
+
+n = int(sys.stdin.readline())
+
+stack = []
+
+for i in range(n):
+    command = sys.stdin.readline().split()
+
+    if command[0] == 'push':
+        stack.append(command[1])
+    elif command[0] == 'pop':
+        if stack:
+            print(stack.pop())
+        else:
+            print(-1)
+    elif command[0] == 'size':
+        print(len(stack))
+    elif command[0] == 'empty':
+        if stack:
+            print(0)
+        else:
+            print(-1)
+    elif command[0] == 'top':
+        if stack:
+            print(stack[-1])
+        else:
+            print(-1)
