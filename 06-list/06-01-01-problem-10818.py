c01d350600cb8857f6d815bf1b89fd93d55fda0c
mal-moo <malinmoohwagwa@gmail.com>
2022-08-25 11:50:17 +0900

    Add 06-01-01-problem-10818.py

---
+++ 06-list/06-01-01-problem-10818.py
@@ -0,0 +1,22 @@
+"""
+https://www.acmicpc.net/problem/10818
+"""
+
+n = int(input())
+array_list = list(map(int, input().split()))
+
+# (1)
+# max_num = array_list[0]
+# min_num = array_list[0]
+
+# for num in array_list:
+#     if num > max_num:
+#         max_num = num
+#     if num < min_num:
+#         min_num = num
+
+# (2)
+max_num = max(array_list)
+min_num = min(array_list)
+
+print(min_num, max_num)
