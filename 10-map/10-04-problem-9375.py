bc397fa70ca933e6342b1724d2fa229747a467a6
mal-moo <malinmoohwagwa@gmail.com>
2022-08-29 23:40:13 +0900

    Add 10-04-problem-9375.py

---
+++ 10-map/10-04-problem-9375.py
@@ -0,0 +1,23 @@
+"""
+    data structure: #map
+    understand: n
+    link: https://www.acmicpc.net/problem/9375
+"""
+
+testcases = int(input())
+
+for i in range(testcases):
+    map = {}
+    answer = 1
+    n = int(input())
+    for j in range(n):
+        a, b = input().split()
+        if not b in map:
+            map[b] = 1
+        else:
+            map[b] += 1
+    
+    for k in map:
+        answer *= map[k] + 1
+
+    print(answer-1)
