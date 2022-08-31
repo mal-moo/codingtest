59cde30355af1c9ea9beb7995c100347cdc678b5
mal-moo <malinmoohwagwa@gmail.com>
2022-08-31 12:32:56 +0900

    Add 10-05-problem-c-1426.py

---
+++ 10-map/10-05-problem-c-1426.py
@@ -0,0 +1,29 @@
+"""
+    data structure: #map
+    understand: y
+    link: https://codeforces.com/problemset/problem/1426/D
+"""
+
+n = int(input())
+a = list(map(int, input().split()))
+
+prefix_sum = [0] * 200001
+map = {}
+answer = 0
+
+map[0] = 1  # 수를 삽입해야하는 연속된 배열의 합이 0일 때를 확인하기 위한 초기화
+
+for i in range(n):
+    prefix_sum[i] = a[i]
+    
+    if i != 0:
+        prefix_sum[i] += prefix_sum[i-1]
+
+    if prefix_sum[i] in map:
+        answer += 1
+        map.clear()
+        map[prefix_sum[i-1]] = 1
+    
+    map[prefix_sum[i]] = 1
+
+print(answer)
